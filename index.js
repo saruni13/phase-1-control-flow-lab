f8ee5f5ea475b7c9595d55af289194c71af2d1eb
saruni <timothysaruni13@gmail.com>
2024-03-19 22:05:03 +0300

    adds controls

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+if(distance<=400){
+  return 'This one is on me!';
+}else if (distance>400 && distance<=2000){
+  return 'That will be twenty bucks'.;
+}else if (distance>2000 && distance<=2500) {
+  return 'i will gladly take your thirsty bucks';
+}else if (distance>2500){
+  return'No can do.';
+ }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city=== 'NYC' ? 'Ok, sounds good.' : 'No, go.';
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(Tip) {
+    case 'generous':
+      return 'Thank you so much';
+      break;
+    case 'not as generous':
+      return 'Thank you';
+      break;
+    default:
+      return 'Bye';
+      break;
+  }
 }
